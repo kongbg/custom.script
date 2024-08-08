c1cc468e1b0fa0cc68d6f9fd78501461d2f241ea
kongbg <kongbage@163.com>
2024-08-08 23:07:39 +0800

    fix: 1

--- onetimeddns.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /tmp/custom.scripts
-date >> ddns.log
-bash ddnspod.sh >> ddns.log
--- updateconfig.sh
+++ updateconfig.sh
@@ -6,9 +6,9 @@
 # 3、拷贝/boot/config/custom.scripts 目录到 /mnt/user/tmp 下
 
 cd /root
-cp -u /root/.ssh/* /boot/config/custom.scripts/.ssh/
-cp -u /root/bash* /boot/config/custom.scripts/
-cp -u /root/zsh* /boot/config/custom.scripts/
+#cp -u /root/.ssh/* /boot/config/custom.scripts/.ssh/
+#cp -u /root/bash* /boot/config/custom.scripts/
+#cp -u /root/zsh* /boot/config/custom.scripts/
 
-tar -zcvf oh-my-zsh.tar.gz .oh-my-zsh
-cp oh-my-zsh.tar.gz /boot/config/custom.scripts/
+#tar -zcvf oh-my-zsh.tar.gz .oh-my-zsh
+#cp oh-my-zsh.tar.gz /boot/config/custom.scripts/
