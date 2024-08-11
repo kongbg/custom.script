cdbd5ec70563c1f67a55d994712f0ca98914afcb
kongbg <kongbage@163.com>
2024-08-11 14:59:11 +0800

    fix: 7

--- setzsh.sh
+++ setzsh.sh
@@ -8,5 +8,11 @@ cd /root
 # 复制zsh 到/bin
 cp -rf /boot/config/unraid-config-back/zsh /bin/
 
+# 赋权限
+sudo chmod +x /bin/zsh
+
+# shells 新增 /bin/zsh
+echo "/bin/zsh" >> /etc/shells
+
 # 修复确实libtinfo.so.5文件问题
 sudo ln -s /usr/lib64/libtinfo.so.6 /usr/lib64/libtinfo.so.5
